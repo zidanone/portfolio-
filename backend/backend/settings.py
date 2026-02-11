ad417014d161106f0fc2fd1433cd147117c7449f
zidanbarkat <135465245+zidanone@users.noreply.github.com>
2026-02-11 22:04:12 +0100

    Update ALLOWED_HOSTS for production environment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-jp-uohuk8f^st6ca6qt-jjgzls
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['https://portfolio-b4be.onrender.com/']
 
 
 # Application definition
